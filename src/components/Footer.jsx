d9807293588d2367b33c3f0e2d7cce73e148ebd5
dteiml <dteiml@gmail.com>
2019-04-10 19:12:38 +0200

    beta deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SFooter = styled.footer`
-	/* position: absolute; */
-	/* left: 0;
-	bottom: 0; */
 	width: 100%;
 	text-align: center;
-	/* height: 60px; */
 	font-size: 18px;
 `
 
